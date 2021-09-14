1c7dc44cd0b8e2bff81d68581148671e251d567b
guilherme-argentino <guilherme.argentino@gympass.com>
2021-09-13 23:42:09 -0300

    Revert "3.5-high-level-exercise: my version"
    This reverts commit 6c8d9f31d3b4a6d4103986f72a369a38f5334c9d.

--- src/main/scala/part3_highlevelserver/HighLevelExercise.scala
+++ src/main/scala/part3_highlevelserver/HighLevelExercise.scala
@@ -1,34 +1,13 @@
 package part3_highlevelserver
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
-import akka.http.scaladsl.Http
+import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.util.Timeout
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives._
-import akka.pattern.ask
-import part3_highlevelserver.PeopleDB.PersonCreated
-import spray.json._
 
-import scala.concurrent.Future
-import scala.concurrent.duration._
-import scala.language.postfixOps
+object HighLevelExercise extends App {
 
-trait PersonStoreJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
-
-  implicit val personFormat: RootJsonFormat[Person] = jsonFormat2(Person.apply)
-  implicit val personCreatedFormat: RootJsonFormat[PersonCreated] = jsonFormat1(PersonCreated)
-
-}
-
-object HighLevelExercise extends App with PersonStoreJsonProtocol {
-
-  implicit val system: ActorSystem = ActorSystem("HighLevelExercise")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-
+  implicit val system = ActorSystem("HighLevelExercise")
+  implicit val materializer = ActorMaterializer()
   import system.dispatcher
-  import PeopleDB._
 
   /**
    * Exercise.
@@ -42,92 +21,11 @@ object HighLevelExercise extends App with PersonStoreJsonProtocol {
    *    - process the entity's data
    */
 
+  case class Person(pin: Int, name: String)
+
   val people = List(
     Person(1, "Alice"),
     Person(2, "Bob"),
     Person(3, "Charlie")
   )
-
-  /*
-    1. setup
-   */
-  val peopleDb = system.actorOf(props = Props[PeopleDB])
-
-  people.foreach { person =>
-    peopleDb ! CreateOrUpdatePerson(person)
-  }
-
-  def toHttpEntity(payload: String) = HttpEntity(ContentTypes.`application/json`, payload)
-
-  implicit val timeout: Timeout = Timeout(2 seconds)
-  val peopleServerRoute =
-    pathPrefix("api" / "people") {
-      get {
-        (path(IntNumber) | parameter('pin.as[Int])) { pin =>
-          complete(
-            (peopleDb ? FindPerson(pin))
-              .mapTo[Option[Person]]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
-        } ~ pathEndOrSingleSlash {
-          complete(
-            (peopleDb ? FindAllPeople)
-              .mapTo[List[Person]]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
-        }
-      } ~ post {
-        entity(as[Person]) { person =>
-          complete(
-            (peopleDb ? CreateOrUpdatePerson(person))
-              .mapTo[PersonCreated]
-              .map(_.toJson.prettyPrint)
-              .map(toHttpEntity)
-          )
-        }
-      }
-    }
-
-  Http().bindAndHandle(peopleServerRoute, "localhost", 8080)
-}
-
-case class Person(pin: Int, name: String)
-
-object PeopleDB {
-
-  case class CreateOrUpdatePerson(person: Person)
-
-  case class PersonCreated(pin: Int)
-
-  case class FindPerson(id: Int)
-
-  case object FindAllPeople
-
-}
-
-class PeopleDB extends Actor with ActorLogging {
-
-  import PeopleDB._
-
-  var people: Map[Int, Person] = Map()
-
-  override def receive: Receive = {
-    case FindAllPeople =>
-      log.info("Searching for all people")
-      sender() ! people.values.toList
-    case FindPerson(pin) =>
-      log.info(s"Searching person by pin: $pin")
-      sender() ! people.get(pin)
-    case CreateOrUpdatePerson(person) =>
-      people.get(person.pin) match {
-        case None =>
-          log.info(s"Adding person $person with pin ${person.pin}")
-        case Some(person) =>
-          log.info(s"Updating person $person with pin ${person.pin}")
-      }
-      people = people + (person.pin -> person)
-      sender() ! PersonCreated(person.pin)
-  }
 }

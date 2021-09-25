33cc4e01e9269880c8e648c408ab802b728b3051
guilherme-argentino <guilherme.argentino@gympass.com>
2021-09-25 19:55:51 -0300

    3.7-marshalling-json-part-2 - final

--- src/main/scala/part3_highlevelserver/MarshallingJSON.scala
+++ src/main/scala/part3_highlevelserver/MarshallingJSON.scala
@@ -3,11 +3,13 @@ package part3_highlevelserver
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.{StatusCode, StatusCodes}
-import akka.http.scaladsl.server.Directives._
-import akka.pattern.ask
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.FromRequestUnmarshaller
+import akka.pattern.ask
 import akka.util.Timeout
+// step 1
 import spray.json._
 
 import scala.concurrent.duration._
@@ -16,23 +18,17 @@ case class Player(nickname: String, characterClass: String, level: Int)
 
 object GameAreaMap {
   case object GetAllPlayers
-
   case class GetPlayer(nickname: String)
-
   case class GetPlayersByClass(characterClass: String)
-
   case class AddPlayer(player: Player)
-
   case class RemovePlayer(player: Player)
-
   case object OperationSuccess
 }
 
 class GameAreaMap extends Actor with ActorLogging {
-
   import GameAreaMap._
 
-  var players = Map[String, Player]()
+  var players: Map[String, Player] = Map[String, Player]()
 
   override def receive: Receive = {
     case GetAllPlayers =>
@@ -59,21 +55,25 @@ class GameAreaMap extends Actor with ActorLogging {
   }
 }
 
+// step 2
 trait PlayerJsonProtocol extends DefaultJsonProtocol {
-  implicit val playerFormat = jsonFormat3(Player)
+  implicit val playerFormat: RootJsonFormat[Player] = jsonFormat3(Player)
 }
 
-object MarshallingJSON extends App with PlayerJsonProtocol with SprayJsonSupport {
+object MarshallingJSON extends App
+  // step 3
+  with PlayerJsonProtocol
+  // step 4
+  with SprayJsonSupport {
 
-  implicit val system = ActorSystem("MarshallingJSON")
-  implicit val materializer = ActorMaterializer()
-
+  implicit val system: ActorSystem = ActorSystem("MarshallingJSON")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
   import system.dispatcher
   import GameAreaMap._
 
   val rtjvmGameMap = system.actorOf(Props[GameAreaMap], "rockTheJVMGameAreaMap")
   val playersList = List(
-    Player("martin_kizz_u", "Warrior", 70),
+    Player("martin_killz_u", "Warrior", 70),
     Player("rolandbraveheart007", "Elf", 67),
     Player("daniel_rock03", "Wizard", 30)
   )
@@ -91,27 +91,35 @@ object MarshallingJSON extends App with PlayerJsonProtocol with SprayJsonSupport
     - (Exercise) DELETE /api/player with JSON payload, removes the player from the map
    */
 
-  implicit val timeout = Timeout(2 seconds)
+  implicit val timeout: Timeout = Timeout(2 seconds)
   val rtjvmGameRouteSkel =
     pathPrefix("api" / "player") {
       get {
-        path("class" / Segment) { charcterClass =>
-          val playersByClassFuture = (rtjvmGameMap ? GetPlayersByClass(charcterClass)).mapTo[List[Player]]
+        path("class" / Segment) { characterClass =>
+          val playersByClassFuture = (rtjvmGameMap ? GetPlayersByClass(characterClass)).mapTo[List[Player]]
           complete(playersByClassFuture)
-        } ~ (path(Segment) | parameter('nickname)) { nickname =>
-          val playerOptionFuture = (rtjvmGameMap ? GetPlayer(nickname)).mapTo[Option[Player]]
-          complete(playerOptionFuture)
-        } ~ pathEndOrSingleSlash {
-          complete((rtjvmGameMap ? GetAllPlayers).mapTo[List[Player]])
-        }
-      } ~ entity(as[Player]) { player =>
+
+        } ~
+          (path(Segment) | parameter('nickname)) { nickname =>
+            val playerOptionFuture = (rtjvmGameMap ? GetPlayer(nickname)).mapTo[Option[Player]]
+            complete(playerOptionFuture)
+          } ~
+          pathEndOrSingleSlash {
+            complete((rtjvmGameMap ? GetAllPlayers).mapTo[List[Player]])
+          }
+      } ~
         post {
-          complete((rtjvmGameMap ? AddPlayer(player)).map(_ => StatusCodes.OK))
-        } ~ delete {
-          complete((rtjvmGameMap ? RemovePlayer(player)).map(_ => StatusCodes.OK))
+          entity(implicitly[FromRequestUnmarshaller[Player]]) { player =>
+            complete((rtjvmGameMap ? AddPlayer(player)).map(_ => StatusCodes.OK))
+          }
+        } ~
+        delete {
+          entity(as[Player]) { player =>
+            complete((rtjvmGameMap ? RemovePlayer(player)).map(_ => StatusCodes.OK))
+          }
         }
-      }
     }
 
   Http().bindAndHandle(rtjvmGameRouteSkel, "localhost", 8080)
+
 }

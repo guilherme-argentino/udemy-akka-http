a9c717c36d68615e90aae6efe07bafcf2ee60011
guilherme-argentino <guilherme.argentino@gympass.com>
2021-09-25 20:27:05 -0300

    3.9-handling-exceptions - final

--- src/main/scala/part3_highlevelserver/HandleExceptions.scala
+++ src/main/scala/part3_highlevelserver/HandleExceptions.scala
@@ -37,6 +37,38 @@ object HandleExceptions extends App {
   }
 
 
-  Http().bindAndHandle(simpleRoute, "localhost", 8080)
+  //  Http().bindAndHandle(simpleRoute, "localhost", 8080)
+
+  val runtimeExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: RuntimeException =>
+      complete(StatusCodes.NotFound, e.getMessage)
+  }
+
+  val noSuchElementExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: NoSuchElementException =>
+      complete(StatusCodes.BadRequest, e.getMessage)
+  }
+
+  val delicateHandleRoute =
+    handleExceptions(runtimeExceptionHandler) {
+      path("api" / "people") {
+        get {
+          // directive that throws some exception
+          throw new RuntimeException("Getting all the people took too long")
+        } ~
+          handleExceptions(noSuchElementExceptionHandler) {
+            post {
+              parameter('id) { id =>
+                if (id.length > 2)
+                  throw new NoSuchElementException(s"Parameter $id cannot be found in the database, TABLE FLIP!")
+
+                complete(StatusCodes.OK)
+              }
+            }
+          }
+      }
+    }
+
+  Http().bindAndHandle(delicateHandleRoute, "localhost", 8080)
 
 }

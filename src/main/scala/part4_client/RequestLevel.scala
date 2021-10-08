2f13cf5a5f55ca7a766d1df782ff44f7e9bf3753
guilherme-argentino <guilherme.argentino@gympass.com>
2021-10-08 11:10:56 -0300

    4.3-request-level-api - final

--- src/main/scala/part4_client/RequestLevel.scala
+++ src/main/scala/part4_client/RequestLevel.scala
@@ -11,8 +11,8 @@ import spray.json._
 
 object RequestLevel extends App with PaymentJsonProtocol {
 
-  implicit val system = ActorSystem("RequestLevelAPI")
-  implicit val materializer = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem("RequestLevelAPI")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
   import system.dispatcher
 
   val responseFuture = Http().singleRequest(HttpRequest(uri = "http://www.google.com"))
@@ -25,5 +25,31 @@ object RequestLevel extends App with PaymentJsonProtocol {
     case Failure(ex) =>
       println(s"The request failed with: $ex")
   }
+
+  import PaymentSystemDomain._
+
+  val creditCards = List(
+    CreditCard("4242-4242-4242-4242", "424", "tx-test-account"),
+    CreditCard("1234-1234-1234-1234", "123", "tx-daniels-account"),
+    CreditCard("1234-1234-4321-4321", "321", "my-awesome-account")
+  )
+
+  val paymentRequests = creditCards.map(creditCard => PaymentRequest(creditCard, "rtjvm-store-account", 99))
+  val serverHttpRequests = paymentRequests.map(paymentRequest =>
+    HttpRequest(
+      HttpMethods.POST,
+      uri = "http://localhost:8080/api/payments",
+      entity = HttpEntity(
+        ContentTypes.`application/json`,
+        paymentRequest.toJson.prettyPrint
+      )
+    )
+  )
+
+  Source(serverHttpRequests)
+    .mapAsyncUnordered(10)(request => Http().singleRequest(request))
+    .runForeach(println)
+
+
 
 }
